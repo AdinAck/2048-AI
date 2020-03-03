031619255850c5d947dc440685fbb67c02f8b81c
AdinAck <adinackerman@gmail.com>
2020-03-02 16:36:56 -0800

    updated train to include "fromModel" option

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -20,18 +20,20 @@ def getOutput(game,w):
     a.shape = 4,4
     return sigmoid(np.sum(a,0))
 
-def train(epochSize, iterations, threshold, randomFactor):
+def train(epochSize, iterations, threshold, randomFactor, modelName=None):
     p = 0
     while os.path.exists("log"+str(p)+".txt"):
         p += 1
     f = open("log"+str(p)+".txt", 'w')
     w = np.ones((hDepth+1,hWidth**2,hWidth**2))
-    wList = np.array([],dtype=np.complex128)
+    if modelName != None:
+        wList = np.load(modelName)
+    else:
+        wList = np.array([],dtype=np.complex128)
     for j in range(iterations):
         scores = np.array([],int)
         game = g.Game()
         o = getOutput(game,w)
-
         for i in range(epochSize):
             game = g.Game()
             # print(game.board)
@@ -59,58 +61,8 @@ def train(epochSize, iterations, threshold, randomFactor):
                             game.move(order[3])
                 o = getOutput(game,w)
             scores = np.append(scores, game.score)
-            if j < 1:
+            if j < 1 and modelName == None:
                 wList = np.append(wList, w)
-        print("Epoch: ",j)
-        print("Worst: ",np.sort(scores)[0])
-        print("Best: ",np.sort(scores)[-1])
-        print("Average: ",np.average(scores))
-        print("====================")
-        f.write(str(j)+"\t"+str(np.sort(scores)[0])+"\t"+str(np.sort(scores)[-1])+"\t"+str(np.average(scores))+"\n")
-        wList = improve(epochSize, wList, scores, threshold, randomFactor)
-    f.close()
-    np.save("model"+str(p)+".npy", wList)
-
-def trainFromModel(modelName,epochSize, iterations, threshold, randomFactor):
-    p = 0
-    while os.path.exists("log"+str(p)+".txt"):
-        p += 1
-    f = open("log"+str(p)+".txt", 'w')
-    w = np.ones((hDepth+1,hWidth**2,hWidth**2))
-    wList = np.load(modelName)
-    for j in range(iterations):
-        scores = np.array([],int)
-        game = g.Game()
-        o = getOutput(game,w)
-
-        for i in range(epochSize):
-            game = g.Game()
-            # print(game.board)
-            game.verbose = False
-            if j >= 1:
-                w = wList[i]
-            while not game.gameEnd:
-                order = np.zeros(4)
-                for k in range(len(order)):
-                    if np.max(o) == o[0]:
-                        order[k] = 0
-                        o[0] = 0
-                    elif np.max(o) == o[1]:
-                        order[k] = 1
-                        o[1] = 0
-                    elif np.max(o) == o[2]:
-                        order[k] = 2
-                        o[2] = 0
-                    elif np.max(o) == o[3]:
-                        order[k] = 3
-                        o[3] = 0
-                if not game.move(order[0]):
-                    if not game.move(order[1]):
-                        if not game.move(order[2]):
-                            game.move(order[3])
-                o = getOutput(game,w)
-            scores = np.append(scores, game.score)
-            
         print("Epoch: ",j)
         print("Worst: ",np.sort(scores)[0])
         print("Best: ",np.sort(scores)[-1])
@@ -132,4 +84,4 @@ def improve(epochSize, wList, scores, threshold,randomFactor):
         if scores[i] != -1:
                 wList[i] = wList[int(bestList[i])] + wList[int(bestList[i])]*randomFactor*np.random.random((hDepth+1,hWidth**2,hWidth**2)) - randomFactor/2
     return wList
-trainFromModel("model7.npy",500,5,2,1)
+train(500,1000,10,10)
